efabe591a5683a1a6cc3901090c63e07724ceb28
zolomohan <mohanzolomon@gmail.com>
2020-12-14 04:12:26 +0530

    Add Query, Update Subscription & Update Layout

--- index.js
+++ index.js
@@ -85,5 +85,71 @@ app.post("/stop", async (req, res) => {
   res.send(stop.data);
 });
 
+app.post("/query", async (req, res) => {
+  const appID = process.env.appID;
+  const resource = req.body.resource;
+  const sid = req.body.sid;
+  const mode = req.body.mode;
+
+  const query = await axios.get(
+    `https://api.agora.io/v1/apps/${appID}/cloud_recording/resourceid/${resource}/sid/${sid}/mode/${mode}/query`,
+    { headers: { Authorization } }
+  );
+
+  res.send(query.data);
+});
+
+app.post("/updateSubscription", async (req, res) => {
+  const appID = process.env.appID;
+  const resource = req.body.resource;
+  const sid = req.body.sid;
+  const mode = req.body.mode;
+
+  const updateSubscription = await axios.get(
+    `https://api.agora.io/v1/apps/${appID}/cloud_recording/resourceid/${resource}/sid/${sid}/mode/${mode}/update`,
+    {
+      cname: req.body.channel,
+      uid: req.body.uid,
+      clientRequest: {
+        streamSubscribe: {
+          audioUidList: req.body.audioSubscription,
+          videoUidList: req.body.videoSubscription,
+        },
+      },
+    },
+    { headers: { Authorization } }
+  );
+
+  res.send(updateSubscription.data);
+});
+
+app.post("/updateLayout", async (req, res) => {
+  const appID = process.env.appID;
+  const resource = req.body.resource;
+  const sid = req.body.sid;
+  const mode = req.body.mode;
+
+  const body = {
+    cname: req.body.channel,
+    uid: req.body.uid,
+    clientRequest: {
+      mixedVideoLayout: req.body.layout,
+      backgroundColor: req.body.backgroundColor,
+    },
+  };
+
+  if (req.body.layoutConfig === 3) {
+    body.clientRequest.layoutConfig = req.body.layoutConfig;
+  }
+
+  const updateLayoutConfig = await axios.get(
+    `https://api.agora.io/v1/apps/${appID}/cloud_recording/resourceid/${resource}/sid/${sid}/mode/${mode}/updateLayout`,
+    body,
+    { headers: { Authorization } }
+  );
+
+  res.send(updateLayoutConfig.data);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Agora Cloud Recording Server listening at Port ${port}`));
